0f98cfb0fd8c887c0e8624069dc69a04194ff467
gizmore <gizmore@wechall.net>
2023-03-21 19:20:15 +0100

    '7.0.2-r1765 Hailing Hermelin'

--- Module_Classic.php
+++ Module_Classic.php
@@ -1,23 +1,24 @@
 <?php
 namespace GDO\Classic;
 
-use GDO\Core\GDO_Module;
-use GDO\UI\GDT_Page;
-use GDO\UI\GDT_Link;
 use GDO\Core\Application;
+use GDO\Core\GDO_Module;
+use GDO\UI\GDT_Link;
+use GDO\UI\GDT_Page;
 
 /**
  * Install this module to load the classic css theme.
- * 
- * @author gizmore
+ *
  * @version 7.0.1
  * @since 6.1.0
+ * @author gizmore
  */
 final class Module_Classic extends GDO_Module
 {
+
 	public int $priority = 50;
-	
-	public function onIncludeScripts() : void
+
+	public function onIncludeScripts(): void
 	{
 		global $me;
 		if (Application::$INSTANCE->hasTheme('classic'))
@@ -32,11 +33,11 @@ final class Module_Classic extends GDO_Module
 			$this->addCSS('css/gdo7-pulse.css');
 		}
 	}
-	
-	public function onInitSidebar() : void
+
+	public function onInitSidebar(): void
 	{
 		GDT_Page::instance()->topBar()->addField(
 			GDT_Link::make('home')->text('sitename')->href(hrefDefault()));
 	}
-	
+
 }
--- README.md
+++ README.md
@@ -4,5 +4,4 @@ A very basic and classic GDOv7 Theme.
 Reminds on jQueryMobile
 It offers left and right nav panels without JS required.
 
-
 ## phpgdo-classic: [GDOv7-LICENSE](LICENSE)
